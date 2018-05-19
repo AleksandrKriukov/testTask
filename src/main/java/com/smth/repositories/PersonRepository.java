eb054d2276b9dc2a41a0aa1778fbb6f4b3b96bd9
AleksandrKriukov <alexanderkry@gmail.com>
2018-05-19 22:57:31 +0300

    fixed bug with db driver

--- src/main/java/com/smth/repositories/PersonRepository.java
+++ src/main/java/com/smth/repositories/PersonRepository.java
@@ -3,8 +3,5 @@ package com.smth.repositories;
 import com.smth.domain.Person;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface PersonRepository extends CrudRepository<Person, Long> {
-    Optional<Person> findById(Long id);
 }
